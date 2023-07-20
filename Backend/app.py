a92cc54aaa1a7924bfc9dac07ea5c23e0e6c7f27
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-07-21 04:57:59 +0530

    fix backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,142 +1,166 @@
-from flask import Flask, request, jsonify
-from pymongo import MongoClient
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
 from bson import ObjectId
 
-# Create the Flask app
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/blissful_abodes'
+mongo = PyMongo(app)
 
-# Configure MongoDB connection
-client = MongoClient('mongodb://localhost:27017')
-db = client['blissful_abodes']
-hosts_collection = db['hosts']
-properties_collection = db['properties']
-guests_collection = db['guests']
-bookings_collection = db['bookings']
 
-# Hosts Endpoints
-@app.route('/api/hosts', methods=['GET'])
+# Hosts
+
+@app.route('/hosts', methods=['GET'])
 def get_hosts():
-    hosts = list(hosts_collection.find())
-    return jsonify(hosts)
+    hosts = mongo.db.hosts.find()
+    return jsonify({'hosts': hosts}), 200
 
-@app.route('/api/hosts/<string:host_id>', methods=['GET'])
+@app.route('/hosts', methods=['POST'])
+def add_host():
+    host = request.json
+    host_id = mongo.db.hosts.insert_one(host).inserted_id
+    host['_id'] = str(host_id)
+    return jsonify({'host': host}), 201
+
+@app.route('/hosts/<string:host_id>', methods=['GET'])
 def get_host(host_id):
-    host = hosts_collection.find_one({'_id': ObjectId(host_id)})
+    host = mongo.db.hosts.find_one({'_id': ObjectId(host_id)})
     if host:
-        return jsonify(host)
+        return jsonify({'host': host}), 200
     return jsonify({'message': 'Host not found'}), 404
 
-@app.route('/api/hosts', methods=['POST'])
-def create_host():
-    data = request.json
-    host_id = hosts_collection.insert_one(data).inserted_id
-    return jsonify({'message': 'Host created', 'host_id': str(host_id)}), 201
-
-@app.route('/api/hosts/<string:host_id>', methods=['PUT'])
+@app.route('/hosts/<string:host_id>', methods=['PUT'])
 def update_host(host_id):
-    data = request.json
-    hosts_collection.update_one({'_id': ObjectId(host_id)}, {'$set': data})
-    return jsonify({'message': 'Host updated'}), 200
+    host = request.json
+    result = mongo.db.hosts.update_one({'_id': ObjectId(host_id)}, {'$set': host})
+    if result.modified_count > 0:
+        host['_id'] = host_id
+        return jsonify({'host': host}), 200
+    return jsonify({'message': 'Host not found'}), 404
 
-@app.route('/api/hosts/<string:host_id>', methods=['DELETE'])
+@app.route('/hosts/<string:host_id>', methods=['DELETE'])
 def delete_host(host_id):
-    hosts_collection.delete_one({'_id': ObjectId(host_id)})
-    return jsonify({'message': 'Host deleted'}), 200
+    result = mongo.db.hosts.delete_one({'_id': ObjectId(host_id)})
+    if result.deleted_count > 0:
+        return jsonify({'message': 'Host deleted'}), 200
+    return jsonify({'message': 'Host not found'}), 404
 
 
-# Properties Endpoints
-@app.route('/api/properties', methods=['GET'])
-def get_properties():
-    properties = list(properties_collection.find())
-    return jsonify(properties)
+# Properties
 
-@app.route('/api/properties/<string:property_id>', methods=['GET'])
-def get_property(property_id):
-    property = properties_collection.find_one({'_id': ObjectId(property_id)})
+@app.route('/hosts/<string:host_id>/properties', methods=['GET'])
+def get_properties(host_id):
+    properties = mongo.db.properties.find({'host_id': host_id})
+    return jsonify({'properties': properties}), 200
+
+@app.route('/hosts/<string:host_id>/properties', methods=['POST'])
+def add_property(host_id):
+    property = request.json
+    property['host_id'] = host_id
+    property_id = mongo.db.properties.insert_one(property).inserted_id
+    property['_id'] = str(property_id)
+    return jsonify({'property': property}), 201
+
+@app.route('/hosts/<string:host_id>/properties/<string:property_id>', methods=['GET'])
+def get_property(host_id, property_id):
+    property = mongo.db.properties.find_one({'_id': ObjectId(property_id), 'host_id': host_id})
     if property:
-        return jsonify(property)
+        return jsonify({'property': property}), 200
     return jsonify({'message': 'Property not found'}), 404
 
-@app.route('/api/properties', methods=['POST'])
-def create_property():
-    data = request.json
-    property_id = properties_collection.insert_one(data).inserted_id
-    return jsonify({'message': 'Property created', 'property_id': str(property_id)}), 201
+@app.route('/hosts/<string:host_id>/properties/<string:property_id>', methods=['PUT'])
+def update_property(host_id, property_id):
+    property = request.json
+    result = mongo.db.properties.update_one({'_id': ObjectId(property_id), 'host_id': host_id}, {'$set': property})
+    if result.modified_count > 0:
+        property['_id'] = property_id
+        return jsonify({'property': property}), 200
+    return jsonify({'message': 'Property not found'}), 404
 
-@app.route('/api/properties/<string:property_id>', methods=['PUT'])
-def update_property(property_id):
-    data = request.json
-    properties_collection.update_one({'_id': ObjectId(property_id)}, {'$set': data})
-    return jsonify({'message': 'Property updated'}), 200
-
-@app.route('/api/properties/<string:property_id>', methods=['DELETE'])
-def delete_property(property_id):
-    properties_collection.delete_one({'_id': ObjectId(property_id)})
-    return jsonify({'message': 'Property deleted'}), 200
+@app.route('/hosts/<string:host_id>/properties/<string:property_id>', methods=['DELETE'])
+def delete_property(host_id, property_id):
+    result = mongo.db.properties.delete_one({'_id': ObjectId(property_id), 'host_id': host_id})
+    if result.deleted_count > 0:
+        return jsonify({'message': 'Property deleted'}), 200
+    return jsonify({'message': 'Property not found'}), 404
 
 
-# Guests Endpoints
-@app.route('/api/guests', methods=['GET'])
+# Guests
+
+@app.route('/guests', methods=['GET'])
 def get_guests():
-    guests = list(guests_collection.find())
-    return jsonify(guests)
+    guests = mongo.db.guests.find()
+    return jsonify({'guests': guests}), 200
 
-@app.route('/api/guests/<string:guest_id>', methods=['GET'])
+@app.route('/guests', methods=['POST'])
+def add_guest():
+    guest = request.json
+    guest_id = mongo.db.guests.insert_one(guest).inserted_id
+    guest['_id'] = str(guest_id)
+    return jsonify({'guest': guest}), 201
+
+@app.route('/guests/<string:guest_id>', methods=['GET'])
 def get_guest(guest_id):
-    guest = guests_collection.find_one({'_id': ObjectId(guest_id)})
+    guest = mongo.db.guests.find_one({'_id': ObjectId(guest_id)})
     if guest:
-        return jsonify(guest)
+        return jsonify({'guest': guest}), 200
     return jsonify({'message': 'Guest not found'}), 404
 
-@app.route('/api/guests', methods=['POST'])
-def create_guest():
-    data = request.json
-    guest_id = guests_collection.insert_one(data).inserted_id
-    return jsonify({'message': 'Guest created', 'guest_id': str(guest_id)}), 201
-
-@app.route('/api/guests/<string:guest_id>', methods=['PUT'])
+@app.route('/guests/<string:guest_id>', methods=['PUT'])
 def update_guest(guest_id):
-    data = request.json
-    guests_collection.update_one({'_id': ObjectId(guest_id)}, {'$set': data})
-    return jsonify({'message': 'Guest updated'}), 200
+    guest = request.json
+    result = mongo.db.guests.update_one({'_id': ObjectId(guest_id)}, {'$set': guest})
+    if result.modified_count > 0:
+        guest['_id'] = guest_id
+        return jsonify({'guest': guest}), 200
+    return jsonify({'message': 'Guest not found'}), 404
 
-@app.route('/api/guests/<string:guest_id>', methods=['DELETE'])
+@app.route('/guests/<string:guest_id>', methods=['DELETE'])
 def delete_guest(guest_id):
-    guests_collection.delete_one({'_id': ObjectId(guest_id)})
-    return jsonify({'message': 'Guest deleted'}), 200
+    result = mongo.db.guests.delete_one({'_id': ObjectId(guest_id)})
+    if result.deleted_count > 0:
+        return jsonify({'message': 'Guest deleted'}), 200
+    return jsonify({'message': 'Guest not found'}), 404
 
 
-# Bookings Endpoints
-@app.route('/api/bookings', methods=['GET'])
+# Bookings
+
+@app.route('/bookings', methods=['GET'])
 def get_bookings():
-    bookings = list(bookings_collection.find())
-    return jsonify(bookings)
+    bookings = mongo.db.bookings.find()
+    return jsonify({'bookings': bookings}), 200
 
-@app.route('/api/bookings/<string:booking_id>', methods=['GET'])
+@app.route('/bookings', methods=['POST'])
+def add_booking():
+    booking = request.json
+    booking_id = mongo.db.bookings.insert_one(booking).inserted_id
+    booking['_id'] = str(booking_id)
+    return jsonify({'booking': booking}), 201
+
+@app.route('/bookings/<string:booking_id>', methods=['GET'])
 def get_booking(booking_id):
-    booking = bookings_collection.find_one({'_id': ObjectId(booking_id)})
+    booking = mongo.db.bookings.find_one({'_id': ObjectId(booking_id)})
     if booking:
-        return jsonify(booking)
+        return jsonify({'booking': booking}), 200
     return jsonify({'message': 'Booking not found'}), 404
 
-@app.route('/api/bookings', methods=['POST'])
-def create_booking():
-    data = request.json
-    booking_id = bookings_collection.insert_one(data).inserted_id
-    return jsonify({'message': 'Booking created', 'booking_id': str(booking_id)}), 201
-
-@app.route('/api/bookings/<string:booking_id>', methods=['PUT'])
+@app.route('/bookings/<string:booking_id>', methods=['PUT'])
 def update_booking(booking_id):
-    data = request.json
-    bookings_collection.update_one({'_id': ObjectId(booking_id)}, {'$set': data})
-    return jsonify({'message': 'Booking updated'}), 200
+    booking = request.json
+    result = mongo.db.bookings.update_one({'_id': ObjectId(booking_id)}, {'$set': booking})
+    if result.modified_count > 0:
+        booking['_id'] = booking_id
+        return jsonify({'booking': booking}), 200
+    return jsonify({'message': 'Booking not found'}), 404
 
-@app.route('/api/bookings/<string:booking_id>', methods=['DELETE'])
+@app.route('/bookings/<string:booking_id>', methods=['DELETE'])
 def delete_booking(booking_id):
-    bookings_collection.delete_one({'_id': ObjectId(booking_id)})
-    return jsonify({'message': 'Booking deleted'}), 200
+    result = mongo.db.bookings.delete_one({'_id': ObjectId(booking_id)})
+    if result.deleted_count > 0:
+        return jsonify({'message': 'Booking deleted'}), 200
+    return jsonify({'message': 'Booking not found'}), 404
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
